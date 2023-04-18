c416610a4dfb94956444e78e0fc4dca47fd83095
KongJinHo <wlsgh7608@naver.com>
2023-04-18 22:49:13 +0900

    로드 밸런서 적용

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,26 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env){
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the First Service";
@@ -22,7 +33,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "HI, there. This is a message from First Servcie";
+    public String check(HttpServletRequest request){
+        log.info("Server port = {}",request.getServerPort());
+        return String.format("HI, there. This is a message from First Servcie on Port %s",env.getProperty("local.server.port"));
     }
 }
