3bde7ba6ec5a9fe904b6caf67d27388e443ab3cf
공진호 <wlsgh7608@naver.com>
2023-04-21 08:28:18 +0900

    Catalog 기능 구현

--- catalog-service/src/main/java/com/example/catalogservice/entity/CatalogEntity.java
+++ catalog-service/src/main/java/com/example/catalogservice/entity/CatalogEntity.java
@@ -29,5 +29,5 @@ public class CatalogEntity implements Serializable {
 
     @Column(nullable = false,updatable = false ,insertable = false)
     @ColumnDefault(value="CURRENT_TIMESTAMP")
-    private Date cratedAt;
+    private Date createdAt;
 }
