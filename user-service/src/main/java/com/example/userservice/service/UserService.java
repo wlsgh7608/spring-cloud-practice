da8a6ada503a127a4536aa4f9c2f8d814c62d52a
KongJinHo <wlsgh7608@naver.com>
2023-04-20 23:42:24 +0900

    사용자 조회 비즈니스로직 추가

--- user-service/src/main/java/com/example/userservice/service/UserService.java
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -1,7 +1,10 @@
 package com.example.userservice.service;
 
 import com.example.userservice.dto.UserDto;
+import com.example.userservice.entity.UserEntity;
 
 public interface UserService {
     UserDto creatdUser(UserDto userDto);
+    UserDto getUserByUserId(String userId);
+    Iterable<UserEntity> getUserByAll();
 }
