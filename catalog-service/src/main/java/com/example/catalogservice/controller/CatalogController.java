a1f947aba7d042c49442a6928e1579d2c439bc21
KongJinHo <wlsgh7608@naver.com>
2023-04-21 00:38:52 +0900

    컨트롤러 작성

---
+++ catalog-service/src/main/java/com/example/catalogservice/controller/CatalogController.java
@@ -0,0 +1,53 @@
+package com.example.catalogservice.controller;
+
+import com.example.catalogservice.entity.CatalogEntity;
+import com.example.catalogservice.service.CatalogService;
+import com.example.catalogservice.vo.ResponseCatalog;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/catalog-service")
+public class CatalogController {
+    Environment env;
+    CatalogService catalogService;
+
+    @Autowired
+    public CatalogController(Environment env, CatalogService catalogService) {
+        this.env = env;
+        this.catalogService = catalogService;
+    }
+
+    @GetMapping("/health_check")
+    public String status() {
+        return String.format("It's Working in User Service on Port %S",
+                env.getProperty("local.server.port"));
+    }
+
+
+    @GetMapping("/catalogs")
+    public ResponseEntity<List<ResponseCatalog>> getCatalogs(){
+        Iterable<CatalogEntity> catalogList = catalogService.getAllCatalogs();
+
+        List<ResponseCatalog> result = new ArrayList<>();
+        catalogList.forEach(v->{
+            result.add(new ModelMapper().map(v, ResponseCatalog.class));
+        });
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+
+    }
+
+
+}
