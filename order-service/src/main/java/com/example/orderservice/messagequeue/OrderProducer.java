cb70f548bed9a6ec904268b3bd9aaea41af55e7f
공진호 <wlsgh7608@naver.com>
2023-04-27 11:17:05 +0900

    오타 수정

--- order-service/src/main/java/com/example/orderservice/messagequeue/OrderProducer.java
+++ order-service/src/main/java/com/example/orderservice/messagequeue/OrderProducer.java
@@ -21,7 +21,7 @@ public class OrderProducer {
             new Field("string", true, "user_id"),
             new Field("string", true, "product_id"),
             new Field("int32", true, "qty"),
-            new Field("int32`", true, "unit_price"),
+            new Field("int32", true, "unit_price"),
             new Field("int32", true, "total_price")
             );
     Schema schema = Schema.builder()
